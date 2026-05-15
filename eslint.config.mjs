7756476c2b62aa5d1d4a58e91ffe2794b86eccf5
Adam Lui <adam@kudoai.com>
2026-05-15 07:19:24 -0700

    Added globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,8 @@ export default [
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
                 ...globals.browser, ...globals.greasemonkey, app: 'writable', chatgpt: 'readonly', css: 'readonly',
-                dom: 'readonly', env: 'writable', settings: 'writable'
+                dom: 'readonly', env: 'writable', feedback: 'readonly', settings: 'writable', sync: 'readonly',
+                toolbarMenu: 'readonly'
             }
         },
         plugins: { 'import': importPlugin, 'js-styles': stylisticJS, regexp },
