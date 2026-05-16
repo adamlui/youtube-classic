d7d322dffb254c6763f7174c161943d4c0711abe
Adam Lui <adam@kudoai.com>
2026-05-16 16:42:59 -0700

    Bumped `@version` to `2026.5.16.5`

--- greasemonkey/youtube-classic.user.js
+++ greasemonkey/youtube-classic.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name              YouTube™ Classic 📺 — (Remove rounded design + Return YouTube dislikes)
-// @version           2026.5.16.4
+// @version           2026.5.16.5
 // @author            Adam Lui, magma_craft
 // @namespace         https://github.com/adamlui
 // @description       Reverts YouTube to its classic design (before all the rounded corners & hidden dislikes) + redirects YouTube Shorts + blocks thumbnail ads
