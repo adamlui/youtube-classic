9a1a22e2fc2cc3eb6a3e336af95bd472662be8c0
adamlui <adam@kudoai.com>
2026-05-22 22:13:03 -0700

    Renamed `checkShortsToRedir()` to `redir.shorts()`, moved `update.headerLogo()` to top level

--- firefox/extension/lib/sync.js
+++ firefox/extension/lib/sync.js
@@ -1,9 +1,11 @@
 window.sync = {
 
-    checkShortsToRedir() {
-        if (location.pathname.startsWith('/shorts/'))
-            return location.replace(`https://www.youtube.com/watch?v=${location.pathname.split('/')[2]}`)
-        window.sync.checkShortsToRedir.id = requestAnimationFrame(window.sync.checkShortsToRedir)
+    redir: {
+        shorts() {
+            if (location.pathname.startsWith('/shorts/'))
+                return location.replace(`https://www.youtube.com/watch?v=${location.pathname.split('/')[2]}`)
+            window.sync.redir.shorts.id = requestAnimationFrame(window.sync.redir.shorts)
+        }
     },
 
     configToUI({ key } = {}) {
@@ -12,15 +14,25 @@ window.sync = {
         else if (key == 'unroundCorners')
             styles.update({ key: 'unround' })
         else if (key == 'disableShorts') {
-            if (app.config.disableShorts && !this.checkShortsToRedir.id)
-                this.checkShortsToRedir()
-            else if (!app.config.disableShorts && this.checkShortsToRedir.id) {
-                cancelAnimationFrame(this.checkShortsToRedir.id) ; this.checkShortsToRedir.id = null }
+            if (app.config.disableShorts && !this.redir.shorts.id)
+                this.redir.shorts()
+            else if (!app.config.disableShorts && this.redir.shorts.id) {
+                cancelAnimationFrame(this.redir.shorts.id) ; this.redir.shorts.id = null }
         } else if (key.endsWith('Block'))
             styles.update({ key: 'block' })
         else if (key == 'idlePrevention')
             this.idle.toUI()
         gmToolbarMenu.refresh() // prefixes/suffixes
+    },
+
+    headerLogo() {
+        app.logo ??= dom.create.elem('img', { style: 'margin-left: 5px', height: 65 })
+        const ytLogo = document.getElementById('logo-icon') ; if (!ytLogo) return
+        const ytScheme = document.querySelector('ytd-masthead[dark]')
+                      || window.matchMedia?.('(prefers-color-scheme: dark)').matches
+                            ? 'dark' : 'light'
+        app.logo.src = `${app.urls.images}/logos/youtube/${ytScheme}mode.png`
+        ytLogo.textContent = '' ; ytLogo.append(app.logo)
     },
 
     idle: {
@@ -46,18 +58,6 @@ window.sync = {
                 delete document.visibilityState
                 delete document.webkitVisibilityState
             }
-        }
-    },
-
-    update: {
-        headerLogo() {
-            app.logo ??= dom.create.elem('img', { style: 'margin-left: 5px', height: 65 })
-            const ytLogo = document.getElementById('logo-icon') ; if (!ytLogo) return
-            const ytScheme = document.querySelector('ytd-masthead[dark]')
-                          || window.matchMedia?.('(prefers-color-scheme: dark)').matches
-                                ? 'dark' : 'light'
-            app.logo.src = `${app.urls.images}/logos/youtube/${ytScheme}mode.png`
-            ytLogo.textContent = '' ; ytLogo.append(app.logo)
         }
     }
 };
