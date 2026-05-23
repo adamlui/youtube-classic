4fccd9fd4cb87c6a552f9524a960c7d7613727d4
adamlui <adam@kudoai.com>
2026-05-23 04:13:14 -0700

    Created bump script (closes adamlui/youtube-classic#190)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default [
         languageOptions: {
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
-                ...globals.browser, ...globals.greasemonkey, app: 'writable', browser: 'readonly',
+                ...globals.browser, ...globals.greasemonkey, ...globals.node, app: 'writable', browser: 'readonly',
                 browserAPI: 'readonly', chatgpt: 'readonly', chrome: 'readonly', css: 'readonly', dom: 'readonly',
                 env: 'writable', feedback: 'readonly', i18n: 'readonly', JSON5: 'readonly', modals: 'writable',
                 settings: 'writable', styles: 'readonly', sync: 'readonly', gmToolbarMenu: 'readonly', ui: 'readonly',
