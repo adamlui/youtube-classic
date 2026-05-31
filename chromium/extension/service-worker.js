af8162710b09f0d517a677628438dbe826cbf76b
adamlui <adam@kudoai.com>
2026-05-31 06:36:32 -0700

    Bumped `app.commitHashes.data` (closes adamlui/youtube-classic#252)

--- chromium/extension/service-worker.js
+++ chromium/extension/service-worker.js
@@ -6,7 +6,7 @@ const ytURL = 'https://www.youtube.com'
     const app = {
         version: chrome.runtime.getManifest().version,
         commitHashes: {
-            data: '36d0ecb', // for <app|selectors|yt-exp-flags>.json
+            data: 'b18f697', // for <app|selectors|yt-exp-flags>.json
             images: 'adb4277' // for header logo
         },
         urls: { jsd: 'https://cdn.jsdelivr.net/gh/adamlui/youtube-classic' }
