da54097c4cafdbe81b88d83cc22765ca11c8e9f8
Adam Lui <adam@kudoai.com>
2026-06-26 23:55:54 -0700

    Secured temp file logic

--- utils/remote-run.js
+++ utils/remote-run.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 
 const { execFileSync } = require('child_process'),
-        fs = require('fs'),
-        path = require('path'),
-        tmp = path.join(require('os').tmpdir(), 'bump.js')
+        fs = require('fs').promises,
+        os = require('os'),
+        path = require('path')
 
 fetch(process.argv[2])
     .then(resp => resp.text())
-    .then(code => {
+    .then(async code => {
+        const tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'remote-run-')),
+              tmpFile = path.join(tmpDir, 'script.js')
         code = code.replace(/^#!.*\r?\n/m, '') // strip shebang
-        fs.writeFileSync(tmp, code)
-        execFileSync(process.execPath, [tmp, ...process.argv.slice(3)], { stdio: 'inherit' })
-        fs.unlinkSync(tmp)
+        await fs.writeFile(tmpFile, code, { mode: 0o600 })
+        try { execFileSync(process.execPath, [tmpFile, ...process.argv.slice(3)], { stdio: 'inherit' }) }
+        finally { await fs.rm(tmpDir, { recursive: true, force: true }) }
     })
     .catch(err => console.error(err))
