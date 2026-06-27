be3a906080bc349e6fb3fb5095c6e6b33b807d38
Adam Lui <adam@kudoai.com>
2026-06-27 00:47:17 -0700

    Replaced `fs.promises` w/ `fs`

--- utils/remote-run.js
+++ utils/remote-run.js
@@ -1,18 +1,21 @@
 #!/usr/bin/env node
 
 const { execFileSync } = require('child_process'),
-        fs = require('fs').promises,
+        fs = require('fs'),
         os = require('os'),
         path = require('path')
 
 fetch(process.argv[2])
     .then(resp => resp.text())
-    .then(async code => {
-        const tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'remote-run-')),
-              tmpFile = path.join(tmpDir, 'script.js')
+    .then(code => {
         code = code.replace(/^#!.*\r?\n/m, '') // strip shebang
-        await fs.writeFile(tmpFile, code, { mode: 0o600 })
-        try { execFileSync(process.execPath, [tmpFile, ...process.argv.slice(3)], { stdio: 'inherit' }) }
-        finally { await fs.rm(tmpDir, { recursive: true, force: true }) }
+        const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'remote-run-')),
+              tmpFile = path.join(tmpDir, 'script.js')
+        try {
+            fs.writeFileSync(tmpFile, code, { encoding: 'utf8', mode: 0o600 })
+            execFileSync(process.execPath, [tmpFile, ...process.argv.slice(3)], { stdio: 'inherit' })
+        } finally {
+            fs.rmSync(tmpDir, { recursive: true, force: true })
+        }
     })
     .catch(err => console.error(err))
