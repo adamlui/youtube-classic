0f60cd50d1ee41e6c2f105bc768251825545a868
Adam Lui <adam@kudoai.com>
2026-06-27 01:13:01 -0700

    Added error log

--- utils/remote-run.js
+++ utils/remote-run.js
@@ -8,12 +8,14 @@ const { execFileSync } = require('child_process'),
 fetch(process.argv[2])
     .then(resp => resp.text())
     .then(code => {
-        code = code.replace(/^#!.*\r?\n/m, '') // strip shebang
+        code = code.replace(/^#!.*\r?\n/m, '') // strip shebangs
         const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'remote-run-')),
               tmpFile = path.join(tmpDir, 'script.js')
         try {
             fs.writeFileSync(tmpFile, code, { encoding: 'utf8', mode: 0o600 })
             execFileSync(process.execPath, [tmpFile, ...process.argv.slice(3)], { stdio: 'inherit' })
+        } catch (err) {
+            console.error(`Error writing/running ${tmpFile}: ${err.message}`)
         } finally {
             fs.rmSync(tmpDir, { recursive: true, force: true })
         }
